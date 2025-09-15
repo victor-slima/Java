f688a2d4df786e68483992b69d94e3bc2044f1eb
victor.slima <victorslima.dev@gmail.com>
2025-09-15 17:39:53 -0300

    primeiro hello word

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// classe pública que pode ser utilizada em outros codigos, desde que seja importada.
+// lembrando que eu to vendo POO na faculdade, entao eu ja sei alguns conceitos de POO.
+// tô só revisando o básico
+public class Main {
+    // Função essencial para projetos que serão executados, EXECUTADOS NO PROPRIO ARQUIVO.
+    public static void main(String[] args){
+        
+        //sysout => tipo o print do python:
+        System.out.println("Hello world.");
+    }
+}
