9153f37690f5caf36adf0c83174fded1a4d7dffa
victor.slima <victorslima.dev@gmail.com>
2025-09-15 17:42:50 -0300

    subino

--- src/olamundo/Main.java
+++ src/olamundo/Main.java
@@ -1,3 +1,7 @@
+// classe esta dentro do pacote olamundo;
+// é tipo os package do netbeans;
+package olamundo;
+
 // classe pública que pode ser utilizada em outros codigos, desde que seja importada.
 // lembrando que eu to vendo POO na faculdade, entao eu ja sei alguns conceitos de POO.
 // tô só revisando o básico
